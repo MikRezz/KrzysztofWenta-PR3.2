653dc7f1fc8d270707eeb3521a77afb10dae636f
MikRezz <krzysztof.wenta13@gmail.com>
2023-01-29 13:06:53 +0100

    Zadanie 1

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,39 @@
 public class Animal {
+    String species;
+    String name;
+    Double weight;
+    Boolean alive;
+
+    public Animal(String species) {
+        this.species = species;
+        this.weight = 3.0;
+        this.alive = true;
+        this.name = "Bambo";
+    }
+
+    public void  feed()
+    {
+        if(this.alive = true && this.weight > 0.5)
+        {
+            this.weight += 0.5;
+            System.out.println("dzieki za jedzenie"+ this.weight);
+        }
+        else
+        {
+            System.out.println("Za późno, zwierzak juz nie żyje !");
+        }
+
+    }
+    public void takeForAWalk(){
+        if(this.alive = true && this.weight > 0.5)
+        {
+            this.weight -= 0.5;
+            System.out.println("dzieki za spacer" + this.weight);
+        }
+        else
+        {
+            System.out.println("zdechł");
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Animal panda = new Animal("canis");
+        panda.feed();
+        panda.takeForAWalk();
+        panda.takeForAWalk();
+        panda.takeForAWalk();
+        panda.takeForAWalk();
+        panda.takeForAWalk();
+        panda.takeForAWalk();
+        panda.takeForAWalk();
+        panda.takeForAWalk();
+        panda.feed();
+        panda.feed();
     }
+
+
 }

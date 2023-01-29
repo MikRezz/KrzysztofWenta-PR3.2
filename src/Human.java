41154cd8aba81cbd18b8276289802f80d8062c6a
MikRezz <krzysztof.wenta13@gmail.com>
2023-01-29 13:27:00 +0100

    Zadanie 3

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,27 @@
 public class Human {
     Animal pet;
     Car car;
+    private Double Salary = 0.0 ;
+
+    public Double getSalary()
+    {
+        return this.Salary;
+    }
+    public void setSalary(Double salary)
+    {
+        if( salary > 0)
+        {
+            this.Salary = salary;
+            System.out.println("-------------------------------------------------------------------- " );
+            System.out.println("Twoja wypłata została zmieniona  na " + salary);
+            System.out.println("-> nowe dane zostały wysłane do systemu księgowego");
+            System.out.println("-> koniecznie odbierz aneks do umowy od pani Hani z kadr");
+            System.out.println("-> ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu");
+            System.out.println("-------------------------------------------------------------------- " );
+        }
+        else
+        {
+            System.out.println("Podaj poprawną wartość !");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,9 @@ public class Main {
         System.out.println(passat.model);
         System.out.println(passat.yearOfProduction);
         System.out.println(passat.millage);
+
+
+
     }
 
 
